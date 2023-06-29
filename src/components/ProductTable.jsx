ab233eff703267bb39e683fea940becee085b2c8
NiNiShyuneLae <heathersan865@gmail.com>
2023-06-29 11:25:21 +0630

    done

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -4,10 +4,10 @@ const ProductTable = ({ item }) => {
   return (
     <>
       <tbody>
-        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+        <tr className="bg-white border-b  ">
           <th
             scope="row"
-            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
           >
             <div className="">
               <h1>{item.name}</h1>
--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -4,10 +4,10 @@ const UserTable = ({ user }) => {
   return (
     <>
       <tbody>
-        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+        <tr className="bg-white border-b  ">
           <th
             scope="row"
-            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
           >
             <div className="flex items-center gap-2">
               <div>
